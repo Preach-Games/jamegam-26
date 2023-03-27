2b222358a0eedaae7939397402ca23e8bf2d4250
CeePritch <charlespritchard.work@gmail.com>
2023-03-27 14:49:38 +0100

    chore: simplify unnecessary garbage code

--- Assets/Scripts/Game/GameStatusHandler.cs
+++ Assets/Scripts/Game/GameStatusHandler.cs
@@ -1,96 +1,43 @@
 ﻿using System;
 using System.ComponentModel;
+using UnityEngine;
+using Object = System.Object;
 
 namespace DungeonDraws.Game
 {
-    public enum GameStatus
-    {
-        Loading,
-        Paused,
-        DayStart,
-        DayReset
-    }
-    
     public sealed class GameStatusHandler
     {
-        private EventHandlerList _gameStatusEvents = new EventHandlerList();
         private static GameStatusHandler instance = null;
         private static readonly object padlock = new object();
-        
-        public event EventHandler OnLoading
-        {
-            add
-            {
-                _gameStatusEvents.AddHandler(GameStatus.Loading, value);
-            }
-            remove
-            {
-                _gameStatusEvents.RemoveHandler(GameStatus.Loading, value);
-            }
-        }
-        
-        public event EventHandler OnPaused
-        {
-            add
-            {
-                _gameStatusEvents.AddHandler(GameStatus.Paused, value);
-            }
-            remove
-            {
-                _gameStatusEvents.RemoveHandler(GameStatus.Paused, value);
-            }
-        }
-        
-        public event EventHandler OnDayStart
-        {
-            add
-            {
-                _gameStatusEvents.AddHandler(GameStatus.DayStart, value);
-            }
-            remove
-            {
-                _gameStatusEvents.RemoveHandler(GameStatus.DayStart, value);
-            }
-        }
-        
-        public event EventHandler OnDayReset
-        {
-            add
-            {
-                _gameStatusEvents.AddHandler(GameStatus.DayReset, value);
-            }
-            remove
-            {
-                _gameStatusEvents.RemoveHandler(GameStatus.DayReset, value);
-            }
-        }
+
+        public event EventHandler OnLoading;
+
+        public event EventHandler OnPaused;
+
+        public event EventHandler OnDayStart;
+
+        public event EventHandler OnDayReset;
 
         public void Load(Object sender, EventArgs e)
         {
-            ProcessEvent(sender, e, GameStatus.Loading);
+            OnLoading?.Invoke(sender, e);
         }
         
         public void Pause(Object sender, EventArgs e)
         {
-            ProcessEvent(sender, e, GameStatus.Paused);
+            OnPaused?.Invoke(sender, e);
         }
 
         public void DayStart(Object sender, EventArgs e)
         {
-            ProcessEvent(sender, e, GameStatus.DayStart);
+            OnDayStart?.Invoke(sender, e);
         }
 
         public void DayReset(Object sender, EventArgs e)
         {
-            ProcessEvent(sender, e, GameStatus.DayReset);
+            OnDayReset?.Invoke(sender, e);
         }
 
-        private void ProcessEvent(Object sender, EventArgs e, GameStatus gs)
-        {
-            EventHandler gameEventDelegate = (EventHandler)_gameStatusEvents[gs];
-            gameEventDelegate(sender, e);
-        }
-        
         private GameStatusHandler()
         {
             //Event handler instanced
