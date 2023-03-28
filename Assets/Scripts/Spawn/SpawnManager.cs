6e90682d13231cd725616c770987324b69327a04
Xwilarg <xwilarg@protonmail.com>
2023-03-29 00:40:32 +0200

    Add alternative spawns

--- Assets/Scripts/Spawn/SpawnManager.cs
+++ Assets/Scripts/Spawn/SpawnManager.cs
@@ -28,6 +28,9 @@ namespace DungeonDraws.Spawn
         [SerializeField]
         private GameObject _character;
 
+        [SerializeField]
+        private Transform[] _spawns;
+
         public float SpawnRate { private set; get; }
 
         private void Awake()
@@ -62,20 +65,12 @@ namespace DungeonDraws.Spawn
 
         public void Spawn(Race race, SpawnMethod spawnMethod)
         {
-            switch (spawnMethod)
-            {
-                case SpawnMethod.Random:
-                    SpawnAtRandom(_info.Enemies.FirstOrDefault(x => x.Race == race));
-                    break;
-                default:
-                    Debug.LogError("Spawn method not implemented: " + spawnMethod);
-                    break;
-            }
+            SpawnAtRandom(_info.Enemies.FirstOrDefault(x => x.Race == race));
         }
 
         private void SpawnAtRandom(SO.CharacterInfo info)
         {
-            Vector2 pos = LevelManager.Instance.PickRandomLocation();
+            Vector2 pos = _spawns[Random.Range(0, _spawns.Length)].position;
             Spawn(info, new Vector3(pos.x, 1f, pos.y));
         }
 
