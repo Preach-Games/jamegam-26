c1d341479a96be04f6496d290b11e29508a152d5
Xwilarg <xwilarg@protonmail.com>
2023-03-29 13:29:19 +0200

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DungeonDraws
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void LoadGame()
+        {
+            SceneManager.LoadScene("Cards");
+        }
+    }
+}
